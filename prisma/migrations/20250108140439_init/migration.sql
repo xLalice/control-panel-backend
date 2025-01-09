9ab12b499f9628f94244c69f5deea37d80c1ac50
John Lorenz <dreamsend08@gmail.com>
2025-01-10 02:40:40 +0800

    Initial commit

---
+++ prisma/migrations/20250108140439_init/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'MARKETING', 'SALES', 'LOGISTICS', 'ACCOUNTING');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SystemSettings" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "description" TEXT,
+    "updatedBy" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "SystemSettings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SystemSettings_key_key" ON "SystemSettings"("key");
---
+++ src/config/passport.ts
@@ -0,0 +1,29 @@
+import passport from "passport";
+import {Strategy as LocalStrategy} from "passport-local";
+import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+passport.use(
+    new LocalStrategy({usernameField: "email"}, async (email: string, password: string, done) => {
+        const user = await prisma.user.findUnique({where: {email}});
+        if (!user){
+            return done(null, false, {message: "Email does not exist."});
+        };
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch){
+            return done(null, false, {message: "Incorrect password"});
+        } else {
+            return done(null, user);
+        }
+}));
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+    const user = await prisma.user.findUnique({where: {id}});
+    done(null, user);
+};
