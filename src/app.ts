37cfee050c702307068104720ff413260cb26ced
John Lorenz Inocentes <geraldinedelima66@gmail.com>
2025-01-20 02:26:27 +0800

    Integrate lead database to app

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import { PrismaClient } from "@prisma/client";
 import authRoutes from "./routes/auth";
 import adminRoutes from "./routes/admin";
+import salesRoutes from "./routes/sales";
 require("dotenv").config();
 
 const SESSION_SECRET = process.env.SESSION_SECRET || "QWERTY"
@@ -62,6 +63,7 @@ app.use((req, res, next) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes)
+app.use("/api/sales", salesRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,14 @@
-import express, {Request, Response } from 'express';
-import { PrismaClient, User } from '@prisma/client';
+import express from 'express';
+import { User } from '@prisma/client';
+import { google } from 'googleapis';
+import fs from "fs";
 
 import passport from "passport"
 require("dotenv").config();
 
 const router = express.Router();
+
+const CREDENTIALS_PATH =  "./src/credentials/credentials.json";
 
 
 router.post('/login', async (req: any, res: any, next) => {
@@ -31,6 +35,39 @@ router.post('/login', async (req: any, res: any, next) => {
       });
     });
   })(req, res, next);
+});
+
+router.get('/google/callback', async (req, res): Promise<any> => {
+  try {
+      console.log("Entered callback");
+
+      const { code } = req.query;
+
+      if (typeof code !== 'string') {
+          return res.status(400).send('Error: Code is missing or invalid');
+      }
+
+      // Get the OAuth2 client - Do not call authenticate here
+      const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, "utf-8"));
+      const { client_secret, client_id, redirect_uris } = credentials.web;
+      const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+      // Exchange the code for an access token
+      const { tokens } = await oAuth2Client.getToken(code);
+
+      // Set the credentials in the OAuth client
+      oAuth2Client.setCredentials(tokens);
+
+      // Save the token to a secure location
+      fs.writeFileSync('token.json', JSON.stringify(tokens));
+
+      console.log('Authentication successful! Token saved.');
+
+      res.status(200).send('Authentication successful!');
+  } catch (error) {
+      console.error('Error during authentication:', error);
+      res.status(500).send('Authentication failed');
+  }
 });
 
 export default router;
