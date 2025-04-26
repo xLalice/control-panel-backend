f3cea546e30849a3bd33184f14ee1d9e5ce2acef
John Lorenz Inocentes <geraldinedelima66@gmail.com>
2025-04-27 02:46:37 +0800

    feat: refactor authentication routes to use controller functions for login and logout

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,51 +1,15 @@
 import express from "express";
 import { User } from "@prisma/client";
 import passport from "passport";
-import { getCurrentUser } from "./auth.controller";
+import { getCurrentUser, loginUser, logoutUser } from "./auth.controller";
+import { log } from "console";
 require("dotenv").config();
 
 const router = express.Router();
 
-router.post("/login", async (req: any, res: any, next) => {
-  passport.authenticate("local", (err: Error, user: User, info: any) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.status(401).json({ message: info.message });
-    }
-    req.logIn(user, (err: Error) => {
-      if (err) {
-        return next(err);
-      }
+router.post("/login", loginUser);
 
-      req.session.save((err: Error) => {
-        if (err) {
-          return next(err);
-        }
-        const { password: _, ...userWithoutPassword } = user;
-        return res.json(userWithoutPassword);
-      });
-    });
-  })(req, res, next);
-});
-
-router.post("/logout", (req: any, res: any) => {
-  req.logout((err: Error) => {
-    if (err) {
-      console.error("Logout error:", err);
-      return res.status(500).json({ message: "Error logging out" });
-    }
-    req.session.destroy((err: Error) => {
-      if (err) {
-        console.error("Error destroying session:", err);
-        return res.status(500).json({ message: "Error destroying session" });
-      }
-      res.clearCookie("connect.sid"); 
-      res.json({ message: "Logged out successfully" });
-    });
-  });
-});
+router.post("/logout", logoutUser);
 
 router.get("/me", getCurrentUser);
 
