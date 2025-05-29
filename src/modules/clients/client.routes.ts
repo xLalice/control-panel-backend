ccd86784d412473614f697e9803b127ebb4d5266
John Lorenz Inocentes <geraldinedelima66@gmail.com>
2025-05-29 23:48:11 +0800

    feat: add client contact history retrieval and update activity log response format

--- src/modules/clients/client.routes.ts
+++ src/modules/clients/client.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createClient, getClient, getClients, updateClient } from "./client.controllers";
+import { createClient, deleteClient, getClient, getClientActivityLog, getClientContactHistory, getClients, updateClient } from "./client.controllers";
 import { isAuthenticated } from "../../middlewares/isAuthenticated";
 
 const router = express.Router();
@@ -7,11 +7,15 @@ const router = express.Router();
 router.get("/", getClients);
 
 router.get("/:id", getClient);
+router.get("/:id/contact-history", getClientContactHistory)
+router.get("/:id/activity-log", getClientActivityLog)
 
 router.post("/", isAuthenticated, createClient);
 
 
 router.patch("/:id", updateClient);
+
+router.delete("/:id", isAuthenticated, deleteClient);
 
 
 export default router;
