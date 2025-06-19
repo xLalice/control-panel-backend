67af782ddabdf4d64c1f9fbf23698cbc9649b2a0
John Lorenz Inocentes <geraldinedelima66@gmail.com>
2025-06-20 02:24:02 +0800

    refactor: simplified error handling by using middleware

--- src/modules/users/user.schema.ts
+++ src/modules/users/user.schema.ts
@@ -1,8 +1,8 @@
-import { z } from "zod/v4";
+import { z } from "zod";
 
 export const CreateUserSchema = z.object({
     name: z.string().min(1, "Name is required"),
-    email: z.email("Invalid email format"),
+    email: z.string().email("Invalid email format"),
     password: z.string().min(6, "Password must be at least 6 characters long"),
     role: z.string().min(1, "Role is required"),
 });
