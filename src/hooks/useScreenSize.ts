5dd1cd8e2c0bcbf3d36b6bea84863e35b3b7c1b7
John Lorenz Inocentes <lorenzinocentes@gmail.com>
2025-11-29 13:53:11 +0800

    feat: add custom hook for screen size detection

---
+++ src/hooks/useScreenSize.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export const useScreenSize = () => {
+    const [screenSize, setScreenSize] = useState("L");
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 640) {
+                setScreenSize("S");
+            } else if (window.innerWidth < 1024) {
+                setScreenSize("M");
+            } else {
+                setScreenSize("L");
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return screenSize;
+};
